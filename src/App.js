5ae64ad02cb06f6fa1c3051a99de4ae6f2a7630c
khaled basta <khaled.basta@protonmail.com>
2022-05-04 16:47:41 +0200

    Add form inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpensesList from "./Components/Expenses/ExpensesList";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -24,7 +25,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <ExpensesList items={expenses} />
     </div>
   );
