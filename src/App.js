f8590f66542d4bd7d1cc6f2340ec9d58d2fe395e
khaled basta <khaled.basta@protonmail.com>
2022-05-04 07:41:42 +0200

    Adjust the date

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2022, 2, 28),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2022, 5, 12),
     },
   ];
   return (
