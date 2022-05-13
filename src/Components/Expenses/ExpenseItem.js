2c1a2a968f99ff78e241be299ca8d128423977e5
khaled basta <khaled.basta@protonmail.com>
2022-05-13 09:08:54 +0200

    Adjust ExpenseItem component

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,23 +4,13 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // we set a default value as props.title
-  // useState return an array with 2 elements the first one is the default value and the second value is a function which we use it to set the new the value
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
