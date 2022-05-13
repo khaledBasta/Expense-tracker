1a7309de3e99092cfb85f8f0020c47065e0e806d
khaled basta <khaled.basta@protonmail.com>
2022-05-13 09:08:12 +0200

    Add Two-way Binding

--- src/Components/Expenses/ExpensesList.js
+++ src/Components/Expenses/ExpensesList.js
@@ -1,31 +1,47 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./ExpensesList.css";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 function ExpensesList(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses-list">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses-list">
+        {/* controlled component Basically, whenever you use two way binding you are controlling a component, value passed to parent through props & value received from the parent component*/}
+        <ExpensesFilter
+          defaultSelected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
