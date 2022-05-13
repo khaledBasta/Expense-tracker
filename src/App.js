0f13b20fb98d840a9f555b3d3122fcb2b9eaf3f6
khaled basta <khaled.basta@protonmail.com>
2022-05-13 10:11:15 +0200

    Add a new Expense dynamically

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
+import { useState } from "react";
 import ExpensesList from "./Components/Expenses/ExpensesList";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2022, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2022, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(`In App.js`);
-    console.log(expense);
+    // Update the state depending on the previous state, you should use this special function form
+    // WE pass a func as an argument to this updating function & that func will automatically receive the latest state snapshot
+    //  That's the cleane way of our state when it's based on an older snapshot of that same state
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+
+    // NOT correct
+    // setExpenses([expense, ...expenses]);
   };
 
   return (
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
