b777bb4d3ee5af95ce76fb45a6dd3e0e3bbbabfe
khaled basta <khaled.basta@protonmail.com>
2022-05-14 17:55:03 +0200

    Rendering conditional content

--- src/Components/Expenses/ExpensesList.js
+++ src/Components/Expenses/ExpensesList.js
@@ -15,6 +15,24 @@ function ExpensesList(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  // You can store JSX content in a variables, u are not limited to using JSX only if u return
+  // You can also use it to create a value, which is stored in a variable
+  let expensesContent = <p>No Expense items found.</p>;
+
+  // Method-03: Write the Logic in the component fucntion itself, and pass only expensesContent inside return
+  // This is probably the cleanest way of all the 3 method
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        // You should always add such a key when mapping out list of items
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses-list">
@@ -23,15 +41,37 @@ function ExpensesList(props) {
           defaultSelected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            // You should always add such a key when mapping out list of items
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+
+        {/* Method-03 */}
+        {expensesContent}
+
+        {/* Mehtod-02: Using AND operator */}
+        {/* {filteredExpenses.length === 0 && <p>No Expense Items Found.</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              // You should always add such a key when mapping out list of items
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+
+        {/* Method-01: Using ternary Operator */}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No Expense Items Found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              // You should always add such a key when mapping out list of items
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
       </Card>
     </div>
   );
